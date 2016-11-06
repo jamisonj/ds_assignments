7f7e80f90b5ee3ef15c12199e5bd633d033744eb
Justin Jamison <Justin Jamison>
2016-11-05 21:24:53 -0600

    Nearly done.

--- reflection/main.php
+++ reflection/main.php
@@ -1,66 +1,86 @@
-# from serialize import to_json
+<?php
 
+include 'serialize.php';
 
 /* Testing Objects */
 
 // A date for a person.
 class Date {
-    function __construct(year, month, day) {
-        $this->year = year;
-        $this->month = month;
-        $this->day = day;
+
+    public $year;
+    public $month;
+    public $day;
+
+    function __construct($year, $month, $day) {
+        $this->year = $year;
+        $this->month = $month;
+        $this->day = $day;
     }
 }
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
 
+// A franchise.
+class Franchise {
 
-class Franchise(object):
-    '''A franchise.'''
-    def __init__(self, name, owner, started):
-        self.name = name
-        self.owner = owner
-        self.started = started
+    public $name;
+    public $owner;
+    public $started;
 
+    function __construct($name, $owner, $started) {
+        $this->name = $name;
+        $this->owner = $owner;
+        $this->started = $started;
+    }
+}
 
-class Person(object):
-    '''A person'''
-    def __init__(self, name, gender, birth_date, is_cool, net_worth, debut_year, father, mother, franchise):
-        self.name = name
-        self.gender = gender
-        self.birth_date = birth_date
-        self.is_cool = is_cool
-        self.net_worth = net_worth
-        self.debut_year = debut_year
-        self.father = father
-        self.mother = mother
-        self.franchise = franchise
+// A franchise.
+class Person {
 
+    public $name;
+    public $gender;
+    public $birth_date;
+    public $is_cool;
+    public $net_worth;
+    public $debut_year;
+    public $father;
+    public $mother;
+    public $franchise;
 
+    function __construct($name, $gender, $birth_date, $is_cool, $net_worth, $debut_year, $father, $mother, $franchise) {
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->birth_date = $birth_date;
+        $this->is_cool = $is_cool;
+        $this->net_worth = $net_worth;
+        $this->debut_year = $debut_year;
+        $this->father = $father;
+        $this->mother = $mother;
+        $this->franchise = $franchise;
+    }
+}
 
-################################################
-###   Main method
+/* Main Method */
 
-if __name__ == '__main__':
-    # person 1
-    fd1 = Date(1962, 8, 1)
-    f1 = Franchise('Spiderman', 'Marvel', fd1)
-    b1 = Date(2011, 2, 3)
-    p1 = Person('Peter "Spidey" Parker', 'M', b1, False, 15000.00, 1967, None, None, f1)
+// Person 1
+$fd1 = new Date(1962, 8, 1);
+$f1 = new Franchise('Spiderman', 'Marvel', $fd1);
+$b1 = new Date(2011, 2, 3);
+$p1 = new Person('Peter "Spidey" Parker', 'M', $b1, false, 15000.00, 1967, null, null, $f1);
 
-    # person 2
-    fd2 = Date(1962, 8, 1)
-    f2 = Franchise('Superman', 'DC\\Comics', fd2)
-    b2 = Date(2014, 5, 6)
-    p2 = Person('Lois Lane', 'F', b2, True, 40000.50, 1981, None, None, f2)
+// Person 2
+$fd2 = new Date(1962, 8, 1);
+$f2 = new Franchise('Superman', 'DC\\Comics', $fd2);
+$b2 = new Date(2014, 5, 6);
+$p2 = new Person('Lois Lane', 'F', $b2, true, 40000.50, 1981, null, null, $f2);
 
-    # person 3
-    fd3 = Date(1963, 1, 1)
-    f3 = Franchise('Doctor Who', 'BBC', fd3)
-    b3 = Date(2017, 8, 9)
-    p3 = Person('River Song/Melody Pond', 'F', b3, True, 91234.56, 2001, p1, p2, f3)
+// Person 3
+$fd3 = new Date(1963, 1, 1);
+$f3 = new Franchise('Doctor Who', 'BBC', $fd3);
+$b3 = new Date(2017, 8, 9);
+$p3 = new Person('River Song/Melody Pond', 'F', $b3, true, 91234.56, 2001, $p1, $p2, $f3);
 
-    # print
-    print(to_json(p3))
+// Print
+//var_dump($p3);
+header('Content-Type: text/plain');
+echo to_json($p3);
+
+?>
---
+++ reflection/serialize.php
@@ -0,0 +1,80 @@
+<?php
+/* Serialization to JSON */
+
+$TAB = '\t';
+
+// Serializes the given object to JSON, printing to the console as it goes.
+function to_json($obj, $level = 0){
+
+	$output = "";
+
+	$tabs = "\t";
+	$tabsless = "";
+
+	for ($c = 0; $c < $level; $c++) {
+		$tabs .= "\t";
+		$tabsless .= "\t";
+	}
+
+    $reflector = new ReflectionClass($obj);
+    $properties = $reflector->getProperties();
+
+	$output .= "{" . "\n";
+    // echo "{" . "\n";
+
+    foreach($properties as $property) {
+
+    	$output .= $tabs . "\"" . $property->getName() . "\": ";
+    	// echo $tabs . "\"" . $property->getName() . "\": ";
+
+    	switch (gettype($property->getvalue($obj))) {
+    		case 'object':
+    			$level++;
+	    		$object = $property->getvalue($obj);
+
+	    		// If value is an object, we need to recursively call our method.
+	    		$output .= to_json($object, $level);
+	    		$level--;
+    			break;
+
+    		case 'boolean':
+    			$output .= $property->getvalue($obj) ? "true" : "false" ;
+    			// echo $property->getvalue($obj) ? "true" : "false" ;
+    			break;
+
+    		case 'string':
+    			$string = addslashes($property->getvalue($obj));
+    			// $string = str_replace('"', '\"', $string);
+    			$output .= "\"" . $string . "\"";
+    			// echo "\"" . $string . "\"";
+    			break;
+
+			case 'double':
+				$output .= number_format($property->getvalue($obj), 2, ".", "");
+				// echo number_format($property->getvalue($obj), 2, ".", "");
+				break;
+
+			default:
+				$value = $property->getvalue($obj);
+
+				// Handles the "null" case as well.
+				$output .= isset($value) ? $value : "null";
+				// echo isset($value) ? $value : "null";
+				break;
+    	}
+
+    	$output .= "," . "\n";
+    	// echo "," . "\n";
+    }
+
+    $output .= $tabsless . "}";
+    // echo $tabsless . "}";
+
+    // echo $tabs . 'Level: ' . $level . "\n";
+
+    // return json_encode($obj, JSON_PRETTY_PRINT) . ",";
+
+    // var_dump($properties);
+
+	return $output;
+}
