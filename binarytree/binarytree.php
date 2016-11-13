fd9edb2133cd8326073320a94d743a74265367c6
Justin Jamison <Justin Jamison>
2016-11-13 00:17:28 -0700

    Updates.

--- binarytree/binarytree.php
+++ binarytree/binarytree.php
@@ -2,7 +2,7 @@
 
 	class BinaryTree {
 		
-		protected $root;
+		public $root;
 
 		function set($key, $value) {
 
@@ -18,42 +18,81 @@
 
 				$selected = $this->root; // 'c'
 
-				$parent = $selected->parent; // '-'
-
 				while ($selected !== '-') {
 
-					echo 'Key: ' . $selected->key . PHP_EOL;
-					
+					// echo 'Starting Key: ' . $selected->key . PHP_EOL;
+
 					// Check if the key is greater than, less than, or equal to the specified key.
 					if ($key < $selected->key) {
-						echo 'Less than ' . $selected->key . PHP_EOL;
-						$parent = $selected;
-						$selected = $selected->left;
+						
+						// echo 'Less than ' . $selected->key . PHP_EOL;
+
+						if ($selected->left == '-') {
+							// echo "Create new node" . PHP_EOL;
+							$node = new Node($key, $value, $selected, '-', '-');
+							$selected->left = $node; // Point $selected left pointer to new node.
+						}
+
+						$selected = $selected->left; // Reassign $selected.
 					}
 
 					elseif ($key > $selected->key) {
-						echo 'More than ' . $selected->key . PHP_EOL;
-						$parent = $selected;
-						$selected = $selected->right;
+						// echo 'More than ' . $selected->key . PHP_EOL;
+
+						if ($selected->right == '-') {
+							// echo "Create new node" . PHP_EOL;
+							$node = new Node($key, $value, $selected, '-', '-');
+							$selected->right = $node; // Point $selected left pointer to new node.
+						}
+
+						$selected = $selected->right; // Reassign $selected.
 					}
 
 					elseif ($key == $selected->key) {
-						echo 'Equal to ' . $selected->key . PHP_EOL;
-						$selected->key = $key;
-						$selected->value = $value;
+						// echo 'Equal to ' . $selected->key . PHP_EOL;
+						$selected = '-';
 					}
-
-					// echo $parent;
 				}
-
-				$selected = new Node($key, $value, $parent, '-', '-');
-
-				echo 'New Key: ' . $selected->key . PHP_EOL;
 			}
 		}
 
 		function get($key) {
 
+			$selected = $this->root; // 'c'
+
+			while ($selected !== '-') {
+
+				// echo 'Starting Key: ' . $selected->key . PHP_EOL;
+
+				$value = $selected->value;
+
+				if ($key == $selected->key) {
+					// echo 'Equal to ' . $selected->key . PHP_EOL;
+					$value = $selected->value;
+					$selected = '-';
+				}
+
+				// Check if the key is greater than, less than, or equal to the specified key.
+				elseif ($key < $selected->key) {
+					// echo 'Less than ' . $selected->key . PHP_EOL;
+					$selected = $selected->left; // Reassign $selected.
+
+					if ($selected == '-') {
+						$value = null;
+					}
+				}
+
+				elseif ($key > $selected->key) {
+					// echo 'More than ' . $selected->key . PHP_EOL;
+					$selected = $selected->right; // Reassign $selected.
+
+					if ($selected == '-') {
+						$value = null;
+					} 
+				}
+			}
+
+			return $value;
 		}
 
 		function remove($key) {
@@ -72,18 +111,114 @@
 
 		}
 
-		function walk_bfs() {
+		function walk_bfs($tree) {
+
+			// $selected = $this->root;
+
+			// yield $selected;
+
+			foreach ($tree as $selected) {
+
+				print_r($selected);
+				// if ($selected->left !== '-') {
+				// 	yield from $this->walk_bfs($selected->left);
+				// 	continue;
+				// }
+
+				// if ($selected->right !== '-') {
+				// 	yield from $this->walk_bfs($selected->right);
+				// 	continue;
+				// }
+			}
+
+			yield $selected->key;
+
+			// yield $selected;
+
+			// yield $selected;
+			
+			// $selected = $this->root; // 'c'
+
+			// $q = new SplQueue();
+			// $q->enqueue($selected);
+
+			// print_r($selected->left);
+			// print_r($selected->right);
+
+			// if (gettype($selected->left == 'object')) {
+			// 	$q->enqueue($selected->left);
+			// 	echo "Called left" . PHP_EOL;
+			// }
+
+			// if (gettype($selected->right == 'object')) {
+			// 	$q->enqueue($selected->left);
+			// 	echo "Called right" . PHP_EOL;
+			// }
+
+			// foreach ($this->generate($selected) as $value) {
+			//     print_r($value);
+			// }
+
+			// foreach ($this->generate(8) as $item) {
+
+			// 	print_r($this->generate($selected));
+				
+			// 	// if ($this->generate($selected->left)) {
+			// 	// 	// $q->enqueue($selected->left);
+			// 	// 	echo "Called left" . PHP_EOL;
+			// 	// }
+
+			// 	// if ($this->generate($selected->right)) {
+			// 	// 	// $q->enqueue($selected->right);
+			// 	// 	echo "Called right" . PHP_EOL;
+			// 	// }
+
+			// 	// print_r($item);
+			// 	// $q->enqueue($item);
+			// }
+
+			
+
+			// foreach ($q as $item) {
+			// 	echo $item->key . PHP_EOL;
+			// }
+
+			// $c = 0;
+
+			
+				// $q->enqueue($this->generate_left($selected->left));
+				// echo $this->generate_left($selected->left)->key;
+				// $q->enqueue($this->generate_right($selected->right));
+				// echo $this->generate_right($selected->right)->key;
+				// // $this->walk_bfs();
+				// $c++;
+				// echo $c . PHP_EOL;
+			
+
+			// print_r($q);
 
 		}
 
 		function debug_print() {
 			$string = '';
 
-			$node = $this->root;
+			$selected = $this->root;
 
-			$string = $node->key . '(' . $node->parent . ')';
+			$string = $selected->key . '(' . $selected->parent . ')';
 
 			return $string;
+		}
+
+		function generate($selected) {
+
+			if (gettype($selected) == 'object') {
+				yield $selected;
+			}
+
+			// if (gettype($selected->right) == 'object') {
+			// 	yield $selected->right;
+			// }
+			
 		}
 	}
 
--- binarytree/main.php
+++ binarytree/main.php
@@ -5,16 +5,29 @@
 	$tree->set('c', 'C');
 
 	echo '<pre>';
-	echo 'Initial Tree:' . PHP_EOL;
-	echo $tree->debug_print() . PHP_EOL;
+	// echo 'Initial Tree:' . PHP_EOL;
+	// echo $tree->debug_print() . PHP_EOL;
 
 	$tree->set('h', 'H');
 	$tree->set('a', 'A');
 	$tree->set('d', 'D');
 	$tree->set('e', 'E');
 	$tree->set('e', 'E');
+	$tree->set('q', 'Q');
+	$tree->set('i', 'I');
+	$tree->set('t', 'T');
 
-	// echo $tree->debug_print() . PHP_EOL;
+
+	echo $tree->get('f');
+
+	echo $tree->debug_print() . PHP_EOL;
+
+	// echo $tree->walk_bfs($tree);
+
+	foreach ($tree->walk_bfs($tree) as $item) {
+		print_r($item);
+	}
+
 	echo '</pre>';
 
 ?>
